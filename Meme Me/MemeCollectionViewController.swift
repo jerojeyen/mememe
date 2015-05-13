aa54d176d1b327bbde01aa7de4577d7ef34edaa7
Jerozan Jeyendrarasa <jerozan.jeyen@gmail.com>
2015-05-13 12:09:23 +0200

    Committed by Udacity

---
+++ Meme Me/MemeCollectionViewController.swift
@@ -0,0 +1,54 @@
+//
+//  MemeCollectionViewController.swift
+//  Meme Me
+//
+//  Created by Jerozan Jeyendrarasa on 5/12/15.
+//  Copyright (c) 2015 Jero Jeyen. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewController: UICollectionViewController {
+    
+    var memes: [Meme]!
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let object = UIApplication.sharedApplication().delegate
+        let appDelegate = object as! AppDelegate
+        memes = appDelegate.memes
+        
+        self.collectionView!.reloadData()
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.memes.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("memeCollectionCellIdentifier", forIndexPath: indexPath)as! MemeCollectionViewCell
+        let meme = self.memes[indexPath.row]
+        
+        cell.img.image = meme.memeImg
+        
+        return cell
+    }
+
+    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        let controller = self.storyboard?.instantiateViewControllerWithIdentifier("memeDetailViewControllerIdentifier")! as! MemeDetailViewController
+        
+        controller.currentMeme = self.memes[indexPath.row]
+        
+        self.navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    @IBAction func addMeme(sender: UIBarButtonItem) {
+        let vc = self.storyboard!.instantiateViewControllerWithIdentifier("memeEditorViewControllerIdentifier")! as! ViewController
+        self.presentViewController(vc, animated: true, completion: nil)
+    }
+}
---
+++ Meme Me/TextFieldDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  TextFieldDelegate.swift
+//  Meme Me
+//
+//  Created by Jerozan Jeyendrarasa on 5/5/15.
+//  Copyright (c) 2015 Jero Jeyen. All rights reserved.
+//
+
+import UIKit
+
+class TextFieldDelegate: NSObject, UITextFieldDelegate {
+    
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        if textField.text == "TOP" || textField.text == "BOTTOM"{
+            textField.text = ""
+        }
+        textField.autocapitalizationType  = UITextAutocapitalizationType.AllCharacters
+        return true
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+}
